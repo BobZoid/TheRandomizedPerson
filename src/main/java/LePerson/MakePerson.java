c2fb4014259547939a23845d0056b0f65d1c6c5f
Martin Siönäs <martin.sionas@gmail.com>
2021-03-31 10:15:08 +0200

    Updated code to use Lists from Google Guava

--- src/main/java/LePerson/MakePerson.java
+++ src/main/java/LePerson/MakePerson.java
@@ -1,16 +1,17 @@
 package LePerson;
 
 
-import java.util.ArrayList;
+import com.google.common.collect.Lists;
+
 import java.util.List;
 
 public class MakePerson {
     public void generateFirstName(UnPerson person) {
-        List<String> listOfNames = new ArrayList<>(List.of("Adrian", "Addison", "Blaine", "Billie", "Charlie", "Chris", "Dane"
-        ,"Dawson", "Easton", "Elliot", "Florian", "Finley", "Gabriel", "Gene", "Harley", "Harlow", "Indigo", "Inga", "Jade", "Joan"
+        List<String> listOfNames = Lists.newArrayList("Adrian", "Addison", "Blaine", "Billie", "Charlie", "Chris", "Dane"
+                ,"Dawson", "Easton", "Elliot", "Florian", "Finley", "Gabriel", "Gene", "Harley", "Harlow", "Indigo", "Inga", "Jade", "Joan"
                 , "Kameron", "Keegan", "Lane", "Lee", "Mackenzie", "Maddox", "Nico", "Noelle", "Ocean", "Owen", "Paige", "Paris"
                 , "Quincy", "Quinn", "Raleigh", "Ramsay", "Salem", "Sawyer", "Tanner", "Tatum", "Umber", "Ulric", "Val", "Venice"
-                , "Wallace", "Wesley", "Zephyr", "Zack"));
+                , "Wallace", "Wesley", "Zephyr", "Zack");
         person.setFirstName(listOfNames.get((int)(Math.random()*48)));
     }
 
@@ -20,17 +21,17 @@ public class MakePerson {
 
 
     public void generateProfession(UnPerson person) {
-        List<String> listOfProfessions = new ArrayList<>(List.of("Banker", "Journalist", "Carpenter", "Plumber", "Baker", "Teacher"
-        , "Police Officer", "Rockstar", "Terrorist", "The Grim Reaper", "Waste Manager", "Drug Dealer", "Barista", "Chef", "Clerk"
-        ,"Garbage Collector"));
+        List<String> listOfProfessions = Lists.newArrayList("Banker", "Journalist", "Carpenter", "Plumber", "Baker", "Teacher"
+        , "Police Officer", "Rockstar", "Terrorist", "Grim Reaper", "Waste Manager", "Drug Dealer", "Barista", "Chef", "Clerk"
+        ,"Garbage Collector");
         person.setProfession(listOfProfessions.get((int) (Math.random()*16)));
     }
 
 
     public void generateHobby(UnPerson person) {
-        List<String> listOfHobbies = new ArrayList<>(List.of("Wargaming", "Dungeons and Dragons", "Guns", "Masturbation", "Horse Riding"
+        List<String> listOfHobbies = Lists.newArrayList("Wargaming", "Dungeons and Dragons", "Guns", "Masturbation", "Horse Riding"
         , "Ant Farms", "Collecting Stamps", "Reading", "Classical Music", "Knitting", "Cooking Meth", "Spoken Word", "Sexual Misconduct"
-                , "Geology", "History", "Arts and Crafts", "Watching TV", "Getting Fat"));
+                , "Geology", "History", "Arts and Crafts", "Watching TV", "Getting Fat");
         person.setHobby(listOfHobbies.get((int) (Math.random()*18)));
     }
 
